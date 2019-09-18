6f67771794f4f0df70c67e77bbb87b93ba09c907
AkwGaniu <lmd4sure@gmail.com>
2019-09-18 23:08:10 +0100

    Inintial commit

---
+++ js/main.js
@@ -0,0 +1,94 @@
+var closeButton = document.querySelector(".close")
+var bgModal = document.querySelector(".bg-modal")
+var books = []
+var booksListing = document.querySelector(".book-listing")
+var inputField = document.querySelector("input")
+
+closeButton.addEventListener("click", () => {
+    bgModal.style.display = "none"
+})
+
+document.querySelector("#add").addEventListener("click", () => {
+    bgModal.style.display = "flex"
+})
+
+document.querySelector(".button").addEventListener("click", () => {
+    //Get the book title
+    let bookName = collectBookTitle();
+    //Hide the modal
+    document.querySelector(".bg-modal").style.display = "none"
+    //Add book to page
+    addBook(bookName)
+    //Clear input field content
+    clear()
+})
+
+//Counter for books added
+let counter = 0
+
+//Function Declaration
+let collectBookTitle = () => {
+    bookTitle = inputField.value
+    console.log(bookTitle)
+    return bookTitle
+}
+
+let addBook = (title) => {
+    counter++
+    books.push(title)
+    let bookContainer = document.createElement('div')
+    bookContainer.innerHTML = `
+        <div class="book" id = "book${counter}">
+            <p contenteditable="true" id = "b-title${counter}">${title}</p>
+            <p class="action">
+                <i class="far fa-window-close" id = "del${counter}" onclick = "delBook(this)"></i>
+            </p>
+        </div>
+    `
+    booksListing.append(bookContainer)
+
+}
+
+let clear = () => {
+    document.querySelector("input").value = ""
+}
+
+let editTitle = (content) => {
+   // let valueToEdit = document.querySelector()
+   
+ 
+   let val = content.parentNode.parentNode.parentNode.firstElementChild.innerText
+    bgModal.style.display =  "flex"
+    inputField.value = val
+
+
+
+
+
+
+}
+
+
+
+let delBook = (del) => {
+    
+    var id = document.getElementById(del.id);
+    id.addEventListener('click', ()=>{
+        var confirmDel = confirm('do you want to delete?');
+        if (confirmDel){
+
+            var toDel = id.parentElement.parentElement.parentElement;
+            toDel.remove();
+        } else {
+            return;
+
+        }
+        
+
+    })
+
+
+
+}
+
+//                 <span  class = "edit" id = "edit${counter}" onclick = "editTitle(this)"><i class="fas fa-edit"></i></span>
