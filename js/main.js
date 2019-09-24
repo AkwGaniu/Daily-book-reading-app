2f83b47a274f669e5e992cbab7e5d2553f4a8313
AkwGaniu <lmd4sure@gmail.com>
2019-09-24 13:28:13 +0100

    Local Sttorage Functionality added

--- js/main.js
+++ js/main.js
@@ -7,107 +7,152 @@ var buton = document.querySelector(".button")
 var val = null
 
 
+document.addEventListener("DOMContentLoaded", e => {    
+    addBookToPage()
+})
+
+
 closeButton.addEventListener("click", () => {
     bgModal.style.display = "none"
     buton.innerHTML = "Add Book"
+    inputField.setAttribute("autofocus", "false")
+
 
 })
 
 document.querySelector("#add").addEventListener("click", () => {
     bgModal.style.display = "flex"
+    inputField.setAttribute("autofocus", "true")
 })
 
 buton.addEventListener("click", () => {
-    //Get the book title
-    let bookName = collectBookTitle();
-    //Hide the modal
-    bgModal.style.display = "none"
+    let  newArray = []
+    let local
+    let bookName = collectBookTitle()  //Get the book title
 
-    //Add book to page
+    bgModal.style.display = "none"      //Hide the modal
 
-    if (val !== null) {
-        val.firstElementChild.innerText = bookName
+    if (val !== null) {     //Add book to page
+
+        let editId = val.getAttribute("id") 
+        editId = editId.substr(4, 2)    //Get the array index from the Id
+        local = localStorageGet()   
+        local = Object.entries(local)
+
+        for ([key, value] of local) {
+            if(key === editId) {
+                local.splice(key, 1, bookName)  //Replace the current item
+                newArray.push(bookName)
+            } else {
+                newArray.push(value)
+            }
+        }
+
+        localStorageSet(newArray)
         val = null
         buton.innerHTML = "Add Book"
-        clearField()
+        clearInputField()
+        document.location.reload(true)
+
     } else {
-        addBook(bookName)
-        clearField()
+
+        addToLocalStorage(bookName)
+        clearInputField()
     }
-    //Clear input field content
 })
 
-//Counter for books added
-let counter = 0
 
 //Function Declaration
-let collectBookTitle = () => {
+const localStorageGet = e => JSON.parse(localStorage.getItem("bookTitle"))
+const localStorageSet = (items) => localStorage.setItem("bookTitle", JSON.stringify(items))
+
+const collectBookTitle = () => {
     bookTitle = inputField.value
     return bookTitle
 }
 
-let addBook = (title) => {
-    counter++
-    books.push(title)
-    let bookContainer = document.createElement('div')
-    bookContainer.innerHTML = `
-        <div class="book" id = "book${counter}">
-            <p contenteditable="true" id = "b-title${counter}">${title}</p>
+const addToLocalStorage = (title) => {
+    let booksToLocalStorage
+    let getItem
+    // Check if Local storage property exists
+    if (localStorage.hasOwnProperty("bookTitle")) {
+
+        getItem = localStorageGet()
+        getItem.push(title)
+        localStorageSet(getItem)
+        document.location.reload(true)  //Reload page to reeflect changes
+
+    } else {
+        books.push(title)
+        localStorageSet(books)
+        document.location.reload(true)  //Reload page to reeflect changes
+    }
+}
+
+const addBookToPage = () => {
+    let bookContainer
+    let existingBooks = localStorageGet()
+
+    existingBooks = Object.entries(existingBooks)   //Convert Local storage entries from objeect to array
+
+    for ([key, value] of existingBooks) {   //Loop through the array and do something
+        
+        bookContainer = document.createElement('div')
+        bookContainer.innerHTML = `
+        <div class="book" id = "book${key}">
+            <p contenteditable="true" id = "b-title${key}">${value}</p>
             <p class="action">
-                <span  class = "edit" id = "edit${counter}" onclick = "editTitle(this)"><i class="fas fa-edit"></i></span>
-                <i class="far fa-window-close" id = "del${counter}" onclick = "delBook(this)"></i>
+                <span  class = "edit" id = "edit${key}" onclick = "editTitle(this)"><i class="fas fa-edit"></i></span>
+                <i class="far fa-window-close" id = "del${key}" onclick = "delBook(this)"></i>
             </p>
         </div>
     `
     booksListing.append(bookContainer)
-
-    console.log(books)
-    localStorage.setItem("booktitle", books)
-
+    }
+    
 }
 
-let clearField = () => {
+const clearInputField = () => {
     document.querySelector("input").value = ""
 }
  
-let editTitle = (content) => {
-   // let valueToEdit = document.querySelector()
-   
- 
-    val = content.parentNode.parentNode.parentNode.firstElementChild
-    let initialCoontent = val.innerText
-    buton.innerHTML = "Change Title"
-    bgModal.style.display =  "flex"
-    inputField.value = initialCoontent
+const delBook = (del) => {
+    let newArray = []
+    let editId
+    let local
+    let confirmDel = confirm('Are you sure you want to Delete this entry?');
 
 
+    val = del.parentNode.parentNode.parentNode.firstElementChild
+    editId = val.getAttribute("id")
+    editId = editId.substr(4, 2)
+    local = localStorageGet()   //Get the Items in the local storage
+    local = Object.entries(local)
 
-
-
-
-}
-
-
-
-let delBook = (del) => {
-    
-    var id = document.getElementById(del.id);
-    id.addEventListener('click', ()=>{
-        var confirmDel = confirm('do you want to delete?');
-        if (confirmDel){
-
-            var toDel = id.parentElement.parentElement.parentElement;
-            toDel.remove();
-        } else {
-            return;
-
+    if (confirmDel){
+        for ([key, value] of local) {
+            if(key === editId) {
+                local.splice(key, 0)
+            } else {
+                newArray.push(value)
+            }
         }
         
-
-    })
-
-
-
+        localStorageSet(newArray)
+        newArray= []
+        document.location.reload(true)
+    } else {
+        return;
+    }
 }
 
-//                 <span  class = "edit" id = "edit${counter}" onclick = "editTitle(this)"><i class="fas fa-edit"></i></span>
+
+
+const editTitle = (content) => {    
+    let = content.parentNode.parentNode.parentNode.firstElementChild
+    let initialCoontent = val.innerText
+
+    buton.innerHTML = "Update"
+    bgModal.style.display =  "flex"
+    inputField.value = initialCoontent
+}
